9edddd84c03f2a8582e775e837e3d3fca4aa124c
rafael <dev.rafael13@gmail.com>
2026-09-12 20:18:33 -0300

    Create ex1.js

---
+++ modulo2/ex1.js
@@ -0,0 +1,9 @@
+// Crie uma variável nota e escreva um if/else que imprima "Aprovado" se a nota for maior ou igual a 6, e "Reprovado" caso contrário.
+
+const nota = 10;
+
+if (nota >= 7) {
+    console.log("Aprovado!");
+} else {
+    console.log("Reprovado!");
+}
